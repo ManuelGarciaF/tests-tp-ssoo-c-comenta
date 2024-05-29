f6f933e012a0fb36a4011d984c4bd556ba1daa84
Manuel Garcia F. <mgarciafrigo@gmail.com>
2024-05-29 10:53:53 -0300

    actualizar scripts

--- finalizar_proceso_io.py
+++ finalizar_proceso_io.py
@@ -1,18 +1,14 @@
 #!/usr/bin/env python3
 
 import time
-import sys
-
-def print_now(str):
-    print(str)
-    sys.stdout.flush()
+import utils
 
 time.sleep(2)
-print_now("INICIAR_PROCESO io.txt")
+utils.print_now("INICIAR_PROCESO io.txt")
 time.sleep(8)
-print_now("PROCESO_ESTADO")
+utils.print_now("PROCESO_ESTADO")
+time.sleep(0.2)
+utils.print_now("FINALIZAR_PROCESO 0")
 time.sleep(1)
-print_now("FINALIZAR_PROCESO 0")
-time.sleep(1)
-print_now("PROCESO_ESTADO")
+utils.print_now("PROCESO_ESTADO")
 time.sleep(3600)
--- finalizar_proceso_new.py
+++ finalizar_proceso_new.py
@@ -1,20 +1,16 @@
 #!/usr/bin/env python3
 
 import time
-import sys
-
-def print_now(str):
-    print(str)
-    sys.stdout.flush()
+import utils
 
 time.sleep(2)
 for i in range(10):
-    print_now("INICIAR_PROCESO loop.txt")
-    time.sleep(1)
-print_now("INICIAR_PROCESO loop.txt") # pid=10
-print_now("PROCESO_ESTADO")
-time.sleep(1)
-print_now("FINALIZAR_PROCESO 10")
-time.sleep(1)
-print_now("PROCESO_ESTADO")
+    utils.print_now("INICIAR_PROCESO loop.txt")
+    time.sleep(0.2)
+utils.print_now("INICIAR_PROCESO loop.txt") # pid=10
+utils.print_now("PROCESO_ESTADO")
+time.sleep(0.2)
+utils.print_now("FINALIZAR_PROCESO 10")
+time.sleep(0.2)
+utils.print_now("PROCESO_ESTADO")
 time.sleep(3600)
